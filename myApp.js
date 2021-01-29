8a392f7a19086188b8c3068cd3e1e40a3a6ab5fd
Anthony Do <anthonyweido@gmail.com>
2021-01-29 08:29:48 +0000

    Basic Node and Express - Use the .env File
    Environmental Vars are crazy

--- myApp.js
+++ myApp.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var app = express();
 
-//console.log("Hello World");
-
-
 var absolutePath = __dirname+ "/views/index.html";
 var assets =  __dirname + "/public";
+
+process.env.MESSAGE_STYLE="uppercase";
 
 app.use("", express.static(assets));
 
@@ -14,13 +13,21 @@ app.get("/", function(req, res){
   res.sendFile(absolutePath);
 });
 
+
+
+
+
+
 app.get("/json", function(req, res){
+if(process.env.MESSAGE_STYLE == "uppercase")
+  res.json({
+    "message" : "HELLO JSON"
+  });
+  else   
   res.json({
     "message" : "Hello json"
   });
 });
-
-
 
 
 
