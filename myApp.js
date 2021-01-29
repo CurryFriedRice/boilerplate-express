323f22d4ad4959169d58e29071ec863c83aee5c9
Anthony Do <anthonyweido@gmail.com>
2021-01-29 08:00:24 +0000

    Basic Node and Express - Serve an HTML File

--- myApp.js
+++ myApp.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var app = express();
 
-console.log("Hello World");
+//console.log("Hello World");
 
-app.get("/", function(req,res){
-  res.send("Hello Express");
+
+var absolutePath = __dirname+ "/views/index.html";
+
+app.get("/", function(req, res){
+  res.sendFile(absolutePath);
 });
-
-
-
 
 
 
