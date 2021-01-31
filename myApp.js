c05f7354669d0c254ca6d166c6221cd188ffe532
Anthony Do <anthonyweido@gmail.com>
2021-01-31 22:54:56 +0000

    Basic Node and Express - Chain Middleware to Create a Time Server

--- myApp.js
+++ myApp.js
@@ -21,6 +21,14 @@ app.get("/", function(req, res){
   res.sendFile(absolutePath);
 });
 
+app.get("/now", function(req, res, next){
+  req.time = new Date().toString();
+  next();
+}, function(req, res){
+  res.json({
+    "time" : req.time
+  });
+});
 
 
 
